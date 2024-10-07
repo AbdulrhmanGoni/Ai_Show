b1188127896fef56e2c6def84326e9faf368ed56
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2024-10-07 21:28:18 +0300

    chore: delete unnecessary files

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { createBrowserRouter, RouterProvider, } from "react-router-dom";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
 import App from './App';
 import "./Ai_Show.css";
 import EventPage from './components/EventPage';
@@ -15,7 +14,7 @@ const router = createBrowserRouter([
   },
   {
     path: "/eventPage/:id",
-    element: <EventPage  />,
+    element: <EventPage />,
   },
 ]);
 
@@ -26,8 +25,3 @@ root.render(
     <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
