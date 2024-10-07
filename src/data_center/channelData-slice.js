67fbb8ad59b875d320a9988b15832d107cd228dc
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2024-10-08 00:17:48 +0300

    refactor: use `import.meta` instead of `process` to access env vars

--- src/data_center/channelData-slice.js
+++ src/data_center/channelData-slice.js
@@ -3,8 +3,8 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 export const fetchChannelData = createAsyncThunk(
     "fetchChannelData",
     async () => {
-        const apiKey = process.env.REACT_APP_API_KEY;
-        const channelId = process.env.REACT_APP_CHANNELID;
+        const apiKey = import.meta.env.VITE_API_KEY;
+        const channelId = import.meta.env.VITE_CHANNELID;
 
         const response = await fetch(`https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics&id=${channelId}&key=${apiKey}`)
         const data = await response.json();
